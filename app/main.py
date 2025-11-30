7b3ec71e0baf630421739babaaee53f1fb1e7276
kaiser1101 <kaiser.philipp95@icloud.com>
2025-11-30 13:17:31 +0100

    created startup and shutdown rout to handle DB

--- app/main.py
+++ app/main.py
@@ -2,14 +2,27 @@
 from fastapi import FastAPI
 from app.ha_client import HAClient
 from app.kostal_service import KostalService
+from app.data_collector import EnergyDataCollector
+from app.inverter_db import init_db
 import logging
 
 app = FastAPI()
 
 ha_client = HAClient()
 kostal_service = KostalService(ha_client)
-
+data_collector = EnergyDataCollector(ha_client)
 log = logging.getLogger("uvicorn.info")
+
+@app.on_event("startup")
+def startup_event():
+    init_db()
+    log.info("SQLite DB initialized")
+    # Start automatic data collection
+    data_collector.start_collection(interval_seconds=15)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    data_collector.stop_collection()
 
 @app.get("/kostal/realtimedata")
 async def realtime_data():
