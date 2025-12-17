a594c1ccb2df7c7bf622b01d58fc33d63666750f
kaiser1101 <kaiser.philipp95@icloud.com>
2025-12-17 12:30:44 +0100

    refactor: use app.state instead of global variables in FastAPI

--- app/main.py
+++ app/main.py
@@ -7,52 +7,49 @@ from app.data_collector import EnergyDataCollector
 from app.inverter_db import init_db
 from app.kostal_service import KostalService
 import logging
+
 app = FastAPI()
-
-ha_client = None
-kostal_service = None
-data_collector = None
-
 log = logging.getLogger("uvicorn.info")
 
 USE_MOCK = True
 
 @app.on_event("startup")
 def startup_event():
-    global ha_client, kostal_service, data_collector
-
     if USE_MOCK:
-        ha_client = None
-        kostal_service = KostalService(ha_client=None)
+        app.state.ha_client = None
+        app.state.kostal_service = KostalService(ha_client=None)
         log.info("MOCK MODE - no DB init, no data collection")
         return
 
-    ha_client = HAClient()
-    kostal_service = KostalService(ha_client)
-    data_collector = EnergyDataCollector(ha_client)
+    app.state.ha_client = HAClient()
+    app.state.kostal_service = KostalService(app.state.ha_client)
+    app.state.data_collector = EnergyDataCollector(app.state.ha_client)
 
     init_db()
     log.info("SQLite DB initialized")
-    data_collector.start_collection(interval_seconds=15)
+    app.state.data_collector.start_collection(interval_seconds=15)
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
     if not USE_MOCK:
-        data_collector.stop_collection()
+        app.state.data_collector.stop_collection()
+
 
 @app.get("/kostal/realtimedata")
 async def realtime_data():
     if USE_MOCK:
         return build_mock_live_json()
-    return await kostal_service.get_realtime_data()
+    return await app.state.kostal_service.get_realtime_data()
+
 
 @app.get("/kostal/lfdata")
 async def lf_data():
     if USE_MOCK:
         return build_mock_interval_json()
-    return await kostal_service.get_lf_data()
+    return await app.state.kostal_service.get_lf_data()
+
 
 @app.get("/kostal/historicaldata")
 async def historic_data():
-    return await kostal_service.get_historical_data()
+    return await app.state.kostal_service.get_historical_data()
